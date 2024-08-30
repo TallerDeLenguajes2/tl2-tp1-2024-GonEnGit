45d5542962e6b3eadfab002b20cebcdd4d994494
Gonzalo Martinez <goncham94@gmail.com>
2024-08-30 20:08:50 -0300

    hechos los metodos del punto 3, falta modificar el programa para que use esos metodos y completar lo que faltaba del tp1

--- Archivos.cs
+++ Archivos.cs
@@ -1,10 +1,43 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
 namespace Archivos;
-/*
-public class string[] Lector()
+
+public class Lector
 {
-    string[] lineas;
+    public virtual string[] LeerArchivo(string archivo)
+    {
+        string[] lineas = {};
+        return lineas;
+    }
+}
 
+public class LectorCSV() : Lector
+{
+    // lo unico que sobreescribis es el cuerpo de la funcion
+    // no podes cambiar la firma de los metodos con el override
+    public override string[] LeerArchivo(string archivo)
+    {
+        string ruta = "csv/";
+        string[] datosDeArchivo = {};
 
+        datosDeArchivo = File.ReadAllLines(ruta + archivo + ".csv");
 
-    return lineas;
-}*/
+        return datosDeArchivo;
+    }
+}
+
+public class LectorJSON() : Lector
+{
+    // lo unico que sobreescribis es el cuerpo de la funcion
+    // no podes cambiar la firma de los metodos con el override
+    public override string[] LeerArchivo(string archivo)
+    {
+        string ruta = "json/";
+        string[] datosDeArchivo = {};
+
+        datosDeArchivo = File.ReadAllLines(ruta + archivo + ".json");
+
+        return datosDeArchivo;
+    }
+}
